54f0e69472d229a188d51f8b52345b370a15d38c
kothamahesh <mahesh96.kotha@gmail.com>
2020-06-26 09:27:17 +0530

    Updates

---
+++ precache-manifest.936896a2be639b97850191893928b3a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6421523f1b07bd8144284c25ca0ff1a",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "514455aa4c41eed190fb",
+    "url": "/react-deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c3deb36ea35ce28b0578",
+    "url": "/react-deploy/static/js/2.efea42fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.efea42fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "514455aa4c41eed190fb",
+    "url": "/react-deploy/static/js/main.77dfbce3.chunk.js"
+  },
+  {
+    "revision": "7f55829ac011f582f216",
+    "url": "/react-deploy/static/js/runtime-main.b4f69db0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
